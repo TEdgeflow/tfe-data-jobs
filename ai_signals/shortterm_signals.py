e57bce7f35e614b0a9446ddecb9e5e1b3dd8ddfd
TEdgeflow <xamilx01@proton.me>
2025-10-02 08:23:33 +0300

    Update shortterm_signals.py

--- ai_signals/shortterm_signals.py
+++ ai_signals/shortterm_signals.py
@@ -66,11 +66,8 @@ def get_latest_signal_inputs(symbol: str, timeframe: str = "5m"):
     delta_score = 1 if delta.data and delta.data[0]["strength_value"] > 0 else 0
     cvd_score = 1 if cvd.data and cvd.data[0]["strength_value"] > 0 else 0
     orderbook_score = 1 if ob.data and ob.data[0]["bid_vol10"] > ob.data[0]["ask_vol10"] else 0
-    liquidation_score = (
-    1 if liq.data and liq.data[0]["long_liquidations"] > liq.data[0]["short_liquidations"] else 0
-    volume_score = 1 if trades.data and (
-    (trades.data[0]["buy_vol"] + trades.data[0]["sell_vol"]) > 1_000_000
-) else 0
+    liquidation_score = 1 if liq.data and liq.data[0]["long_liquidations"] > liq.data[0]["short_liquidations"] else 0
+    volume_score = 1 if trades.data and (trades.data[0]["buy_vol"] + trades.data[0]["sell_vol"]) > 1_000_000 else 0
     
     return {
         "symbol": symbol,
